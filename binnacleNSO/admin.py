4471782e626a25bced2e87e83253055749a70706
UMRGRS <115056186+UMRGRS@users.noreply.github.com>
2024-04-26 12:41:40 -0600

    Base files

--- binnacleNSO/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from . import models
-# Register your models here.
-
-admin.site.register(models.Superuser)
-admin.site.register(models.Subuser)
-admin.site.register(models.Entry)
