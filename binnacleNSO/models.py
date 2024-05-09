9ada4c08d572a7fd0343919d67b8a033d0d05808
UMRGRS <qxrvynilp@gmail.com>
2024-05-08 20:13:05 -0600

    Image saving corrected

--- binnacleNSO/models.py
+++ binnacleNSO/models.py
@@ -13,9 +13,10 @@ from django.utils.deconstruct import deconstructible
 from django_resized import ResizedImageField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.conf import settings
 
-usersPhotosFolder = 'media/usersPhotos'
-entriesPhotosFolder = 'media/entriesPhotos'
+usersPhotosFolder = 'usersPhotos'
+entriesPhotosFolder = 'entriesPhotos'
 
 # Create your models here.
 @deconstructible
@@ -23,7 +24,7 @@ class UploadToPathAndRename(object):
 
     def __init__(self, path):
         self.sub_path = path
-
+        
     def __call__(self, instance, filename):
         ext = filename.split('.')[-1]
         filename = '{}.{}'.format(uuid4().hex, ext)
@@ -53,7 +54,7 @@ class Profile(models.Model):
     
 class Alias(models.Model):
     name = models.CharField(_('Nombre'), unique=True, max_length=40, blank=False)
-    image = ResizedImageField(_('Imagen'), size=[200,200], upload_to=UploadToPathAndRename(usersPhotosFolder), keep_meta=False, force_format='JPEG', default='media/usersPhotos/default.jpg')
+    image = ResizedImageField(_('Imagen'), size=[200,200], upload_to=UploadToPathAndRename(os.path.join(settings.MEDIA_ROOT, usersPhotosFolder)), keep_meta=False, force_format='JPEG', default='media/usersPhotos/default.jpg')
     profile = models.ForeignKey(('Profile'), on_delete=models.CASCADE, related_name='profile')
     
     def __str__(self):
@@ -67,10 +68,10 @@ class Entry(models.Model):
     content = models.TextField(_('Contenido'), max_length=200, blank=False)
     stars = models.PositiveIntegerField(_('Estrellas'), default=1)
     shares = models.PositiveIntegerField(_('Retweets'), default=1)
-    image = models.ImageField(_('Imagen'), upload_to=UploadToPathAndRename(entriesPhotosFolder), blank=True, null=True)
+    image = models.ImageField(_('Imagen'), upload_to=UploadToPathAndRename(os.path.join(settings.MEDIA_ROOT, entriesPhotosFolder)), blank=True, null=True)
     day = models.PositiveIntegerField(_('Numero de entrada'), default=1)
-    profile = models.ForeignKey(('Profile'), on_delete=models.CASCADE, related_name='entry')
-    alias = models.ForeignKey(('Alias'), on_delete=models.CASCADE, related_name='entry')
+    profile = models.ForeignKey(('Profile'), on_delete=models.CASCADE)
+    alias = models.ForeignKey(('Alias'), on_delete=models.CASCADE)
     
     def save(self, *args, **kwargs):
         if not self.pk:
@@ -93,7 +94,7 @@ class Entry(models.Model):
 @receiver(post_save, sender=Entry)
 def modifyEntryImage(sender, instance, created, **kwargs):
     if(created and instance.image.name != None):
-        img = cv2.imread(instance.image.name)
+        img = cv2.imread(os.path.join(settings.MEDIA_ROOT, instance.image.name))
         filename = instance.image.name.split('/')[-1]
         res = cv2.xphoto.oilPainting(img, 9, 2)
-        cv2.imwrite(os.path.join(entriesPhotosFolder, filename), res)
+        cv2.imwrite(os.path.join(settings.MEDIA_ROOT, entriesPhotosFolder, filename), res)
--- binnacleNSO/tests/testsModels.py
+++ binnacleNSO/tests/testsModels.py
@@ -6,67 +6,69 @@ from datetime import date, timedelta
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import override_settings
 
-from binnacleNSO.models import Superuser, Subuser, Entry
+from binnacleNSO.models import Profile, Alias, Entry
 
-TEST_DIR = 'test_data'
+TEST_DIR = 'media'
 
 # Create your tests here.
-class SuperuserTestCases(TestCase):
-    def testModifySuperuserName(self):
-        user = Superuser.objects.create(name='UMRGRS')
-        self.assertEqual(user.name, '@UMRGRS')
+class ProfileTestCases(TestCase):
+    def testModifyProfileName(self):
+        user = Profile.objects.create(name='UMRGRS')
+        self.assertEqual(user.name, 'UMRGRS')
         
     def testResetStreakFalse(self):
-        user = Superuser.objects.create(name='UMRGRS', lastEntryDate=date.today(), streak=10)
+        user = Profile.objects.create(name='UMRGRS', lastEntryDate=date.today(), streak=10)
         user.resetStreak()
         self.assertEqual(user.streak, 10)
     
     def testResetStreakTrue(self):
-        user = Superuser.objects.create(name='UMRGRS', lastEntryDate=date.today()-timedelta(days=2), streak=10)
+        user = Profile.objects.create(name='UMRGRS', lastEntryDate=date.today()-timedelta(days=2), streak=10)
         user.resetStreak()
         self.assertEqual(user.streak, 0)
         
     def testAddEntry(self):
-        sub = Subuser.objects.create(name='UMRGRS', superuser=Superuser.objects.create(name='UMRGRS'))
-        Entry.objects.create(content='OwO', subuser=sub)
-        user = Superuser.objects.get(name='@UMRGRS')
+        sub = Alias.objects.create(name='UMRGRS', profile=Profile.objects.create(name='UMRGRS'))
+        Entry.objects.create(content='OwO', profile=Profile.objects.get(name='UMRGRS'), alias=sub)
+        user = Profile.objects.get(name='UMRGRS')
         self.assertEqual(user.streak, 1)
         self.assertEqual(user.numberOfEntries, 1)
         self.assertEqual(user.lastEntryDate, date.today())
         
-class SubuserTestCases(TestCase):
-    def testCreateSubuserNameWithoutPhoto(self):
-        sub = Subuser.objects.create(name='UMRGRS', superuser=Superuser.objects.create(name='UMRGRS'))
-        self.assertEqual(sub.name, '@UMRGRS')
+class AliasTestCases(TestCase):
+    def testCreateAliasNameWithoutPhoto(self):
+        sub = Alias.objects.create(name='UMRGRS', profile=Profile.objects.create(name='UMRGRS'))
+        self.assertEqual(sub.name, 'UMRGRS')
         self.assertEqual(sub.image, 'media/usersPhotos/default.jpg')
     
-    @override_settings(MEDIA_ROOT=(TEST_DIR + '/media/usersPhotos'))
-    def testCreateSubuserNameWitPhoto(self):
-        sub = Subuser.objects.create(name='UMRGRS', image=SimpleUploadedFile(name='test_image.jpg', content=open('binnacleNSO/tests/testPhotos/test.jpg', 'rb').read(), content_type='image/jpeg'), superuser=Superuser.objects.create(name='UMRGRS'))
+    @override_settings(MEDIA_ROOT=(TEST_DIR))
+    def testCreateAliasNameWitPhoto(self):
+        sub = Alias.objects.create(name='UMRGRS', image=SimpleUploadedFile(name='test_image.jpg', content=open('binnacleNSO/tests/testPhotos/test.jpg', 'rb').read(), content_type='image/jpeg'), profile=Profile.objects.create(name='UMRGRS'))
         self.assertRegex(sub.image.name, '^media/usersPhotos/')
        
 class EntriesTestCases(TestCase):
     def testCreateEntryWithoutPhoto(self):
-        sub = Subuser.objects.create(name='UMRGRS', superuser=Superuser.objects.create(name='UMRGRS'))
-        entry = Entry.objects.create(content='OwO', subuser=sub)
-        user = Superuser.objects.get(name='@UMRGRS')
+        profile = Profile.objects.create(name='UMRGRS')
+        alias = Alias.objects.create(name='UMRGRS', profile=profile)
+        entry = Entry.objects.create(content='OwO', profile=profile, alias=alias)
+        user = Profile.objects.get(name='UMRGRS')
         self.assertEqual(entry.image, None)
         self.assertEqual(entry.day, user.numberOfEntries)
-        self.assertEqual(entry.subuser.superuser, user)
+        self.assertEqual(entry.profile, user)
     
-    @override_settings(MEDIA_ROOT=(TEST_DIR + '/media/entriesPhotos'))
+    @override_settings(MEDIA_ROOT=(TEST_DIR))
     def testCreateEntryWithPhoto(self):
-        sub = Subuser.objects.create(name='UMRGRS', superuser=Superuser.objects.create(name='UMRGRS'))
-        entry = Entry.objects.create(content='OwO', image=SimpleUploadedFile(name='test_image.jpg', content=open('binnacleNSO/tests/testPhotos/test.jpg', 'rb').read(), content_type='image/jpeg'), subuser=sub)
-        user = Superuser.objects.get(name='@UMRGRS')
+        profile = Profile.objects.create(name='UMRGRS')
+        alias = Alias.objects.create(name='UMRGRS', profile=profile)
+        entry = Entry.objects.create(content='OwO', image=SimpleUploadedFile(name='test_image.jpg', content=open('binnacleNSO/tests/testPhotos/test.jpg', 'rb').read(), content_type='image/jpeg'), profile=profile, alias=alias)
+        user = Profile.objects.get(name='UMRGRS')
         self.assertRegex(entry.image.name, '^media/entriesPhotos/')
         self.assertEqual(entry.day, user.numberOfEntries)
-        self.assertEqual(entry.subuser.superuser, user)
+        self.assertEqual(entry.profile.name, user.name)
         
 def tearDownModule():
     print ('\nDeleting temporary files..\n')
     #shutil.rmtree(TEST_DIR, ignore_errors=True)
-    try:
-        shutil.rmtree(TEST_DIR)
-    except OSError:
-        pass
+    #try:
+    #    shutil.rmtree(TEST_DIR)
+    #except OSError:
+    #    pass
